880503c3903097b2a5e98759976902ac6efb7240
Mohammad <aali361@gmail.com>
2022-02-14 02:26:31 +0330

    feat: add admin title

--- analyzer/analyzer_app/urls.py
+++ analyzer/analyzer_app/urls.py
@@ -3,8 +3,12 @@ from django.urls import path, re_path, include
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("secret-admin/", admin.site.urls),
     re_path("api/(?P<version>(v1|v2))/", include("analyzer.urls")),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+
+admin.site.site_header = "Analyzer Administration Panel"
+admin.site.index_title = "Analyzer"
+admin.site.site_title = "Analyzer Admin"
