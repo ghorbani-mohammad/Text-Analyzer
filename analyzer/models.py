415483cbe0f669e04132167e990f9b4ef588a197
Mohammad <aali361@gmail.com>
2022-02-13 04:13:46 +0330

    feat: update gitignore feat: black format code

--- analyzer/models.py
+++ analyzer/models.py
@@ -23,17 +23,23 @@ class News(models.Model):
     link = models.CharField(max_length=250)
     created_at = models.DateTimeField()
     authors = ArrayField(models.CharField(max_length=70))
-    
+
     class Meta:
-       managed = False
-       db_table = 'news'
-    
+        managed = False
+        db_table = 'news'
+
     def __str__(self):
         return '{}. {}'.format(self.id, self.title)
 
+
 class Operation(BaseModel):
-    news_id = models.OneToOneField('News', related_name='operations', related_query_name='operation',
-                                 on_delete=models.CASCADE, db_column='news_id')
+    news_id = models.OneToOneField(
+        'News',
+        related_name='operations',
+        related_query_name='operation',
+        on_delete=models.CASCADE,
+        db_column='news_id',
+    )
     keyword = models.BooleanField(default=False)
     ner = models.BooleanField(default=False)
     category = models.BooleanField(default=False)
@@ -54,50 +60,70 @@ class Option(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-       managed = False
-       db_table = 'options'
-    
+        managed = False
+        db_table = 'options'
+
     def __str__(self):
         return '{}'.format(self.key)
-    
+
 
 class Keyword(models.Model):
-    news_id = models.OneToOneField('News', related_name='keywords', related_query_name='keyword',
-                                 on_delete=models.CASCADE, db_column='news_id')
+    news_id = models.OneToOneField(
+        'News',
+        related_name='keywords',
+        related_query_name='keyword',
+        on_delete=models.CASCADE,
+        db_column='news_id',
+    )
     keyword = models.CharField(max_length=70)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     news_date = models.DateTimeField()
 
     class Meta:
-       managed = False
-       db_table = 'news_keyword'
-    
+        managed = False
+        db_table = 'news_keyword'
+
     def __str__(self):
         return '{}'.format(self.id)
 
 
 class Ner(models.Model):
-    news_id = models.OneToOneField('News', related_name='ners', related_query_name='ner',
-                                 on_delete=models.CASCADE, db_column='news_id')
+    news_id = models.OneToOneField(
+        'News',
+        related_name='ners',
+        related_query_name='ner',
+        on_delete=models.CASCADE,
+        db_column='news_id',
+    )
     entity = models.CharField(max_length=70)
     type = models.CharField(max_length=70)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-       managed = False
-       db_table = 'news_ner'
-    
+        managed = False
+        db_table = 'news_ner'
+
     def __str__(self):
         return '{}'.format(self.id)
 
 
 class Geo(models.Model):
-    news_id = models.OneToOneField('News', related_name='geos', related_query_name='geo',
-                                 on_delete=models.CASCADE, db_column='news_id')
-    ner_id = models.OneToOneField('Ner', related_name='geos', related_query_name='geo',
-                                 on_delete=models.CASCADE, db_column='ner_id')
+    news_id = models.OneToOneField(
+        'News',
+        related_name='geos',
+        related_query_name='geo',
+        on_delete=models.CASCADE,
+        db_column='news_id',
+    )
+    ner_id = models.OneToOneField(
+        'Ner',
+        related_name='geos',
+        related_query_name='geo',
+        on_delete=models.CASCADE,
+        db_column='ner_id',
+    )
     location = models.CharField(max_length=70)
     country = models.CharField(max_length=70)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -105,16 +131,21 @@ class Geo(models.Model):
     news_date = models.DateTimeField()
 
     class Meta:
-       managed = False
-       db_table = 'ner_country'
-    
+        managed = False
+        db_table = 'ner_country'
+
     def __str__(self):
         return '{}'.format(self.id)
-                        
+
 
 class Sentiment(models.Model):
-    news_id = models.OneToOneField('News', related_name='sentiments', related_query_name='sentiment',
-                                 on_delete=models.CASCADE, db_column='news_id')
+    news_id = models.OneToOneField(
+        'News',
+        related_name='sentiments',
+        related_query_name='sentiment',
+        on_delete=models.CASCADE,
+        db_column='news_id',
+    )
     neg = models.FloatField(default=0.0)
     pos = models.FloatField(default=0.0)
     neu = models.FloatField(default=0.0)
@@ -123,91 +154,115 @@ class Sentiment(models.Model):
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-       managed = False
-       db_table = 'news_sentiment'
-    
+        managed = False
+        db_table = 'news_sentiment'
+
     def __str__(self):
         return '{}'.format(self.id)
 
 
 class Doc2vec(models.Model):
-    news_id = models.OneToOneField('News', related_name='doc2vecs', related_query_name='doc2vec',
-                                 on_delete=models.CASCADE, db_column='news_id')
+    news_id = models.OneToOneField(
+        'News',
+        related_name='doc2vecs',
+        related_query_name='doc2vec',
+        on_delete=models.CASCADE,
+        db_column='news_id',
+    )
     vector = JSONField()
     vector_norm = models.FloatField(default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-       managed = False
-       db_table = 'news_vector'
-    
+        managed = False
+        db_table = 'news_vector'
+
     def __str__(self):
         return '{}'.format(self.id)
 
 
 class Related(models.Model):
-    news_id = models.OneToOneField('News', related_name='relateds', related_query_name='related',
-                                 on_delete=models.CASCADE, db_column='news_id')
+    news_id = models.OneToOneField(
+        'News',
+        related_name='relateds',
+        related_query_name='related',
+        on_delete=models.CASCADE,
+        db_column='news_id',
+    )
     related_news_id = models.BigIntegerField()
     score = models.FloatField(default=0.0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
-       managed = False
-       db_table = 'news_related'
-    
+        managed = False
+        db_table = 'news_related'
+
     def __str__(self):
         return '{}'.format(self.id)
 
 
 class ArmyCategory(models.Model):
-	label = models.CharField(max_length=70)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-    
-	class Meta:
-		managed = False
-		db_table = 'army_category'
+    label = models.CharField(max_length=70)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        managed = False
+        db_table = 'army_category'
 
 
 class ArmyKeyword(models.Model):
-	label = models.CharField(max_length=70)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
+    label = models.CharField(max_length=70)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-	class Meta:
-		managed = False
-		db_table = 'army_keyword'
+    class Meta:
+        managed = False
+        db_table = 'army_keyword'
 
 
 class CategoryKeyword(models.Model):
-	army_category_id = models.OneToOneField('ArmyCategory', related_name='categorykeywords',
-	                                        related_query_name='categorykeyword', on_delete=models.CASCADE,
-	                                        db_column='army_category_id')
+    army_category_id = models.OneToOneField(
+        'ArmyCategory',
+        related_name='categorykeywords',
+        related_query_name='categorykeyword',
+        on_delete=models.CASCADE,
+        db_column='army_category_id',
+    )
+    army_keyword_id = models.OneToOneField(
+        'ArmyKeyword',
+        related_name='keywords',
+        related_query_name='keyword',
+        on_delete=models.CASCADE,
+        db_column='army_keyword_id',
+    )
 
-
-	army_keyword_id = models.OneToOneField('ArmyKeyword', related_name='keywords',
-	                                        related_query_name='keyword', on_delete=models.CASCADE,
-	                                        db_column='army_keyword_id')
-	class Meta:
-		managed = False
-		db_table = 'category_keyword'
+    class Meta:
+        managed = False
+        db_table = 'category_keyword'
 
 
 class NewsCategory(models.Model):
-	news_id = models.OneToOneField('News', related_name='news', related_query_name='news',
-	                               on_delete=models.CASCADE, db_column='news_id')
+    news_id = models.OneToOneField(
+        'News',
+        related_name='news',
+        related_query_name='news',
+        on_delete=models.CASCADE,
+        db_column='news_id',
+    )
+    army_category_id = models.OneToOneField(
+        'ArmyCategory',
+        related_name='newscategories',
+        related_query_name='newscategory',
+        on_delete=models.CASCADE,
+        db_column='army_category_id',
+    )
+    score = models.FloatField(null=True, blank=True, default=None)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
-
-	army_category_id = models.OneToOneField('ArmyCategory', related_name='newscategories', related_query_name='newscategory',
-	                               on_delete=models.CASCADE, db_column='army_category_id')
-
-	score = models.FloatField(null=True, blank=True, default=None)
-	created_at = models.DateTimeField(auto_now_add=True)
-	updated_at = models.DateTimeField(auto_now=True)
-
-	class Meta:
-		managed = False
-		db_table = 'news_category'
+    class Meta:
+        managed = False
+        db_table = 'news_category'
