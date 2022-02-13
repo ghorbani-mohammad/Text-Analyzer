b6a5c5878cdc6440c41ebfdb4dbf5c370e14f381
Mohammad <aali361@gmail.com>
2022-02-13 05:33:21 +0330

    feat: add new keyword extraction

---
+++ analyzer/keyword/m_keyword.py
@@ -0,0 +1,239 @@
+# %%
+
+
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+import  pandas as pd
+import os
+import sys
+from collections import OrderedDict
+import numpy as np
+from parsivar import Tokenizer
+from parsivar import *
+from parsivar import POSTagger
+from parsivar import Normalizer
+
+# %%
+
+normalizer = Normalizer()
+my_tagger = POSTagger()
+tokenizer = Tokenizer()
+
+
+# %%
+
+
+class TextRank4Keyword:
+    """Extract keywords and keyphrase from text"""
+
+    def __init__(self):
+        self.d = 0.85  # damping coefficient, usually is .85
+        self.min_diff = 1e-5  # convergence threshold  0.00001
+        self.steps = 10  # iteration steps
+        self.node_weight = None  # save keywords and its weight
+        self.normalizer = Normalizer()
+        self.stop_words = [line.rstrip('/n') for line in open('./stop_words.txt', encoding="utf8")]
+
+    def normalize_text(self, text):
+        return self.normalizer.normalize(text)
+
+    def get_sentences(self, file_text):
+        return tokenizer.tokenize_sentences(file_text)
+
+    def prepare_sentences(self, sentences, candid_pos):
+        pured_sentences = []
+        for sentence in sentences:
+            pured_sentence = []
+            tokens = tokenizer.tokenize_words(sentence)
+
+            tags = my_tagger.parse(tokens)
+            for i, token in enumerate(tokens):
+                if token in self.stop_words:
+                    continue
+                else:
+                    if tags[i][1] in candid_pos:
+                        token = token.replace(u'/u200c', ' ')
+                        pured_sentence.append(token)
+            pured_sentences.append(pured_sentence)
+        return pured_sentences
+
+    def get_vocab(self, sentences):
+        """Get all tokens"""
+        vocab = OrderedDict()
+        i = 0
+        for sentence in sentences:
+            for word in sentence:
+                if word not in vocab:
+                    vocab[word] = i
+                    i += 1
+        return vocab
+
+    def get_token_pairs(self, window_size, sentences):
+        """Build token_pairs from windows in sentences"""
+        token_pairs = list()
+        for sentence in sentences:
+            for i, word in enumerate(sentence):
+                for j in range(i + 1, i + window_size):
+                    if j >= len(sentence):
+                        break
+                    pair = (word, sentence[j])
+                    if pair not in token_pairs:
+                        token_pairs.append(pair)
+        return token_pairs
+
+    def symmetrize(self, a):
+        return a + a.T - np.diag(a.diagonal())
+
+    def get_matrix(self, vocab, token_pairs):
+        """Get normalized matrix"""
+        vocab_size = len(vocab)
+        g = np.zeros((vocab_size, vocab_size), dtype='float')
+        for word1, word2 in token_pairs:
+            i, j = vocab[word1], vocab[word2]
+            g[i][j] = 1
+        g = self.symmetrize(g)
+        norm = np.sum(g, axis=0)
+        g_norm = np.divide(g, norm, where=norm != 0)  # this is ignore the 0 element in norm
+        return g_norm
+
+    def calculate_score(self, vocab, matrix):
+        pr = np.array([1] * len(vocab))
+        previous_pr = 0
+        for epoch in range(self.steps):
+            pr = (1 - self.d) + self.d * np.dot(matrix, pr)
+            if abs(previous_pr - sum(pr)) < self.min_diff:
+                break
+            else:
+                previous_pr = sum(pr)
+        node_weight = dict()
+        for word, index in vocab.items():
+            node_weight[word] = pr[index]
+        return node_weight
+
+    def get_keywords(self, node_weights, number=3):
+        # out_file = open('WordOut_' + file_name, 'w', encoding="utf8")
+        node_weight = OrderedDict(sorted(node_weights.items(), key=lambda t: t[1], reverse=True))
+        ##print('Keywords')
+        keywords=[]
+        for i, (key, value) in enumerate(node_weight.items()):
+            # out_file.write(key + ' - ' + str(value)+'/n')
+            keywords.append(key)
+            ##print(key)
+            if i > number:
+                break
+        return keywords
+
+    def get_phrases(self, sentences, candid_pattern):
+        all_candid_phrases = []
+        for sentence in sentences:
+            sentence_candid_phrases = []
+            tokens = tokenizer.tokenize_words(sentence)
+            tagss = my_tagger.parse(tokens)
+            #print(tagss)
+
+            candid_phrase = []
+            for i, token in enumerate(tokens):
+                if (token not in self.stop_words) and (tagss[i][1] in candid_pattern):
+                    token = token.replace(u'/u200c', ' ')
+                    candid_phrase.append(token)
+                else:
+                    if len(candid_phrase) > 1 and (tagss[i][1] == 'N_SING' or tagss[i][1] == 'Ne' or True):
+                        sentence_candid_phrases.append(candid_phrase)
+                    candid_phrase = []
+            all_candid_phrases.append(sentence_candid_phrases)
+        return all_candid_phrases
+
+    def score_phrase(self, all_phrases, scores):
+        phrases_score = dict()
+        for sentence_phrases in all_phrases:
+            for phrase in sentence_phrases:
+                score = 0
+                phrase_string = ''
+                count = len(phrase)
+                for word in phrase:
+                    score = score + scores[word]
+                    phrase_string += ' ' + word
+                phrases_score[phrase_string] = score / count
+        return phrases_score
+
+    def get_keyphrases(self, phrase_weights, number=1):
+        # out_file = open('PhraseOut_' + file_name, 'w', encoding="utf8")
+        node_weight = OrderedDict(sorted(phrase_weights.items(), key=lambda t: t[1], reverse=True))
+        ##print('keypharase')
+        key_ph=[]
+        for i, (key, value) in enumerate(node_weight.items()):
+            # out_file.write(key + ' - ' + str(value) + '/n')
+
+            key_ph.append(key)
+            if i > number:
+                break
+        return key_ph
+        # out_file.close()
+
+
+def analyze_files(text):
+    tr4w = TextRank4Keyword()
+    text_normalized =text
+    sentences = tr4w.get_sentences(text_normalized)
+    sentences_processed = tr4w.prepare_sentences(sentences, ['N','Ne','N_SING','ADJ','Aje','AJ', "ADV_COMP"])
+    vocab = tr4w.get_vocab(sentences_processed)
+    token_pairs = tr4w.get_token_pairs(4, sentences_processed)
+    matrix = tr4w.get_matrix(vocab, token_pairs)
+    scores = tr4w.calculate_score(vocab, matrix)
+    kw=tr4w.get_keywords( scores)
+    phrases = tr4w.get_phrases(sentences, [ 'ADJ','N_SING'])
+    phrases_score = tr4w.score_phrase(phrases, scores)
+    kp=tr4w.get_keyphrases( phrases_score)
+    return kw,kp
+
+
+def keyword_keyphrase():
+  key_phrase={}
+  key_words={}
+  all_twiit=[]
+  number_file=0
+  import  string
+  alpha=list(string.ascii_lowercase)+list(string.ascii_uppercase)
+  alpha.append('@')
+  alpha.append('#')
+
+  key_phrase = {}
+  key_words = {}
+
+  data=pd.read_excel('./input/formaled_data.xlsx')
+  for row in range(data.shape[0]):
+      
+      com=str(data['stemmer'].iloc[row])
+      com=com.replace("_"," ")
+      kw, kp = analyze_files(com)
+      for k in kp:
+
+          if k in key_phrase:
+              key_phrase[k] += 1
+          else:
+              if  list(filter(k.startswith, alpha)) != [] :
+                  continue
+                  #print("yes")
+              else:
+                  key_phrase[k] = 1
+
+      for k in kw:
+
+          if k in key_words:
+              key_words[k] += 1
+          else:
+              if  list(filter(k.startswith, alpha)) != []   :
+                   continue
+
+              else:
+                  key_words[k] = 1
+  final_keywords={key:key_words[key] for key in key_words if key_words[key]>2 and len(key)>=2 }
+  df = pd.DataFrame(key_phrase.items())
+
+  df.to_excel("./output/keyphrases.xlsx")
+
+  df = pd.DataFrame(final_keywords.items())
+  df.to_excel("./output/keywords.xlsx")
+
+keyword_keyphrase()
