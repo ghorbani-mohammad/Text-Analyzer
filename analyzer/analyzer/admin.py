b1857ad3ac578a5edbfad90bde75da112169b6ae
Mohammad <aali361@gmail.com>
2022-02-15 01:34:47 +0330

    feat: minor change

--- analyzer/analyzer/admin.py
+++ analyzer/analyzer/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Operation)
+class OperationAdmin(admin.ModelAdmin):
+    list_display = (
+        "pk",
+        "news_id",
+        "keyword",
+        "ner",
+        "category",
+        "sentiment",
+        "doc2vec",
+        "related_news",
+        "geo",
+        "summary",
+        "created_at",
+    )
