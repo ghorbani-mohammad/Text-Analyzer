fce572aa59c043af96f1175dcb178064b7265c28
Mohammad <aali361@gmail.com>
2022-02-15 01:28:09 +0330

    feat: minor change

--- analyzer/analyzer/tasks.py
+++ analyzer/analyzer/tasks.py
@@ -290,7 +290,8 @@ def news_doc2vec():
     spacy_model = AnalyzerConfig.spacy_model
     doc2vec_analyzer_algorithm = Option.objects.get(key="doc2vec_analyzer").value
     news = Operation.objects.filter(doc2vec=False).order_by("-id")
-    for item in news[:50]:
+    print(news.count())
+    for item in news[: min(50, len(news.count()))]:
         now = time.strftime("%Y-%m-%d %H:%M:%S")
         with transaction.atomic():
             Doc2vec.objects.filter(news_id=item.news_id.id).delete()
