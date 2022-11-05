4eff888f37841220fd20af76832f525e8e0547e5
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-05 21:09:25 +0330

    minor change

--- analyzer/analyzer_app/settings.py
+++ analyzer/analyzer_app/settings.py
@@ -87,11 +87,11 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-LANGUAGE_CODE = "en-us"
 TIME_ZONE = "UTC"
 USE_I18N = True
 USE_L10N = True
 USE_TZ = False
+LANGUAGE_CODE = "en-us"
 
 
 # Static files (CSS, JavaScript, Images)
