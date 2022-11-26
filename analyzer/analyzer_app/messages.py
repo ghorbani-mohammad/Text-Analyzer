fbf6d39e4d7bb52ede8f2daf334e672d27f94788
Mohammad <mohammad.ghorbani@hotmail.com>
2022-11-26 18:30:12 +0330

    minor change

--- analyzer/analyzer_app/messages.py
+++ analyzer/analyzer_app/messages.py
@@ -8,8 +8,8 @@ msg = {
             "success_crawl_agency_activeAll": "تمامی آژانس های خبری فعال شدند",
             "success_crawl_all": "کرال تمامی صفحات در صف انجام عملیات قرار گرفت",
             "success_crawl_agency_disableAll": "تمامی آژانس های خبری غیر فعال شدند",
-            "success_crawl_agency": "کرال آژانس مورد نظر در صف انجام عملیات قرار گرفت",
             "success_crawl_page": "کرال صفحه ی مورد نظر در صف انجام عملیات قرار گرفت",
+            "success_crawl_agency": "کرال آژانس مورد نظر در صف انجام عملیات قرار گرفت",
         },
         "report": {
             "report_found": "گزارش مورد نظر یافت شد",
