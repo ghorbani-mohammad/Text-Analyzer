19d1701e2d50d8b520adb3335916be105ad1a19d
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-12 23:56:26 +0330

    minor change

--- analyzer/analyzer/utils.py
+++ analyzer/analyzer/utils.py
@@ -5,7 +5,7 @@ REDIS_CLIENT = redis.Redis(host="analyzer_redis", port=6379, db=5)
 
 def only_one_concurrency(function=None, key="", timeout=None):
     def _dec(run_func):
-        def _caller(*args, **kwargs):
+        def wrapper(*args, **kwargs):
             have_lock = False
             lock = REDIS_CLIENT.lock(key, timeout=timeout)
             try:
@@ -16,6 +16,6 @@ def only_one_concurrency(function=None, key="", timeout=None):
                 if have_lock:
                     lock.release()
 
-        return _caller
+        return wrapper
 
     return _dec(function) if function is not None else _dec
